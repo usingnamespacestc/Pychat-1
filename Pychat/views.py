78c19ee8e25a6ee864f0561d32a890479a5f5a2d
xw <1204327445@qq.com>
2020-07-04 23:28:57 +0800

    添加登录功能

--- Pychat/views.py
+++ Pychat/views.py
@@ -7,10 +7,17 @@ def home(request):
     if request.method == 'GET':
         return render(request, 'index.html')
     elif request.method == 'POST':
-        user_name = request.POST['Username']
-        password1 = request.POST['Password1']
-        password2 = request.POST['Password2']
-        if(user_name and password1 and password1):
+        try:
+            user_name = request.POST['Username']
+            password1 = request.POST['Password1']
+            password2 = request.POST['Password2']
+        except BaseException:
+            user_name3 = request.POST['Username3']
+            password3 = request.POST['Password3']
+            user = auth.authenticate(username=user_name3, password=password3)
+            if user is None:
+                return render(request, 'index.html', {'错误': '用户名或密码错误'})
+        else:
             try:
                 User.objects.get(username=user_name)
                 return render(request, 'index.html', {'用户名错误': '该用户名已存在'})
@@ -21,4 +28,3 @@ def home(request):
                     return render(request, 'index.html')
                 else:
                     return render(request, 'index.html', {'密码错误': '两次密码不同'})
-        
