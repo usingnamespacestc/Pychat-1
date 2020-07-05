2982d04d905a58b4dd1d8a1fac637c36026de9c8
xw <1204327445@qq.com>
2020-07-05 18:24:15 +0800

    连接了Mysql，但app修改不了数据库

--- Pychat/settings.py
+++ Pychat/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Pychat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'NAME': 'user_data',
+        'USER': 'root',
+        'PASSWORD': '990712',
     }
 }
 
